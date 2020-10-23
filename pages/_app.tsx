b2d61c85c4d38acfd2e6ce8b35ece14fe3d20f39
Benoit TRAVERS <benoit.travers.fr@gmail.com>
2020-10-24 00:41:55 +0200

    :art: move js files to ts

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react'
+import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
